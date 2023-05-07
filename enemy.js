4cb9f8756bad6759b6c3d47a7f244ec0b283b336
Anirudh-030307 <65300694+Anirudh-030307@users.noreply.github.com>
2023-05-07 20:52:56 +0530

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -6,12 +6,10 @@ AFRAME.registerComponent("enemy-fireballs", {
     shootEnemyMonster: function () {
         var scene = document.querySelector("#scene");
 
-        //enemyMonster entity
         var enemyMonster = document.querySelectorAll(".enemy");   
             
         for (var i = 0; i < enemyMonster.length; i++) {
 
-       //create fireballs
         var fireball = document.createElement("a-entity");
 
         fireball.setAttribute("class","fireball")
@@ -42,20 +40,15 @@ AFRAME.registerComponent("enemy-fireballs", {
         player.getWorldPosition(position1);
         enemy_fireball.getWorldPosition(position2);
 
-        //set the velocity and it's direction
         var direction = new THREE.Vector3();
 
         direction.subVectors(position1, position2).normalize();
 
         fireball.setAttribute("velocity", direction.multiplyScalar(20));
 
-        /******************************************************************************************* */
-
-        //check player life
         var element = document.querySelector("#countLife");
         var playerLife = parseInt(element.getAttribute("text").value);
 
-        //collide event on enemy bullets
         fireball.addEventListener("collide", function (e) {
            
             if (e.detail.body.el.id === "weapon") {               
@@ -70,7 +63,6 @@ AFRAME.registerComponent("enemy-fireballs", {
                     var txt = document.querySelector("#over");
                     txt.setAttribute("visible", true);
 
-                    //remove monsters
                     var El = document.querySelectorAll(".enemy")
                     for (var i = 0; i < El.length; i++) {
                         scene.removeChild(El)
@@ -83,6 +75,4 @@ AFRAME.registerComponent("enemy-fireballs", {
 
     }
     },
-    
-
 });
